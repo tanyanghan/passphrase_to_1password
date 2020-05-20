dac55769fb990c5e16f6432c49c3723f0fa09955
Tan Yang Han <yanghantan@automatic.com>
2020-05-20 19:20:58 +0800

    Clean up code, add README.md and LICENSE

---
+++ README.md
@@ -0,0 +1,111 @@
+# Extracting Phabricator Passphrase and importing into 1Password
+
+For whatever reason, you may find yourself needing to extract the whole Phabricator Passphrase dataabse and importing it into your 1Password.
+
+For this, you will need to have:
+1. SSH access to the server instance where your Phabricator is hosted
+2. Access rights to be able to view Phabricator configuration files
+3. With the above two, you can view the login information to the mysql database
+4. mrc-converter-suite
+
+## Proceed with caution, caveat emptor
+
+Though the following steps are safe if you follow it exactly, you can do ***very bad things*** if you don't know what you are doing while logged into your Phabricator server instance or the mysql database.
+
+I will take no responsibility if you wipe out your Phabricator server, mysql database, or you let your plain text password mysql dump file fall into the wrong hands while following these instructions. 
+
+## mrc-converter-suite
+
+You'll need to grab this 1Password community tool from: 
+
+https://discussions.agilebits.com/discussion/30286/mrcs-convert-to-1password-utility-mrc-converter-suite
+
+This tool is required because it allows custom fields to be added to your 1Password entry that you are importing.
+
+Unzip the zipfile into the same directory as this Git checkout so that `mrc-converter-suite` is a directory under the Git repo.
+
+You will need Perl available to use this tool, which I assume you do.
+
+## Finding Phabrictor's settings file
+
+The first thing is to find out your Phabricator's MySQL login information. You can easily do this by looking in the `local.json` configuration file for your Phabricator. This is usually found in the directory `/phabricator/conf/local/local.json`. 
+
+The four fields in `local.json` you are interested in are:
+```
+    "mysql.host" - eg. mysql.abc.com
+    "mysql.port" - eg. 3306
+    "mysql.pass" - eg. my_p@ssw0rd
+    "mysql.user" - eg. phabricator
+```
+
+## Extracting the databases from MySQL
+
+Once you have the above information, you can use `mysqldump` to grab the two databases that you will need. The two databases you are interested in are:
+1. phabricator_passphrase
+2. phabricator_user
+
+The user database is used to reference the full name of the Passphrase entry's Author, which we'll add to the 1Password entry so that we'll know who created the entry.
+
+The commands for `mysqldump` are as follows, using the example values given above:
+```
+    mysqldump --user=phabricator --password --host mysql.abc.com --port 3306 phabricator_passphrase > phabricator_passphrase.sql
+    mysqldump --user=phabricator --password --host mysql.abc.com --port 3306 phabricator_user > phabricator_user.sql
+```
+
+Be very careful with securing the file `phabricator_passphrase.sql`, as all the passwords in the file are not encrypted and anyone who gets a copy of the file will be able to read it like an open book.
+
+Transfer these two `.sql` files to the same directory as this Git checkout. I used scp. 
+
+## Converting Passphrase to 1Password using passphrase_to_1password.py
+
+Finally, after all the above, you will be ready to use this Python script to do the conversion and output a `.1pif` file that can be imported into 1Password.
+
+The most basic call you will need to do is:
+
+```
+    python3 passphrase_to_1password.py -p phabricator_passphrase.sql -u phabricator_user.sql
+```
+
+This will output two files named:
+1. `output_<date and timestamp>.csv`
+2. `output_<date and timestamp>.1pif`
+
+The `.csv` file is an intermediate file that is fed to mrc-converter-suite and can be deleted. 
+
+The `.1pif` file is the one you need to import into 1Password. 
+
+If you are having problems, a couple useful options available to help are:
+```
+  -s, --save_intermediate_file
+                        Flag to saves the intermediate data as json files (for
+                        debug)
+  -d {DEBUG,INFO,ERROR}, --debug_level {DEBUG,INFO,ERROR}
+                        Select the debug level (default level INFO)
+```
+
+The `-s` flag will save the itermediate json files for inspection.
+
+The `-d` option allows you to select `DEBUG` logging level for more information about the fields found for each table.
+
+## What gets imported into 1Password
+
+The following fields are imported into 1Password:
+1. Title - The title will be the combined Passphrase index number, with `K` prepended to it, together with the `name` of the Passphrase entry.
+2. Username
+3. Password - This will depend on the `CredentialType` of the Passphrase entry, in my instance, there were five different types. More below.
+4. Author - The name of the author of the Passphrase entry.
+5. Created - This is the timestamp when the Passphrase entry was created. 
+6. Modified - This is the last time anyone interacted with the Passphrase entry (including viewing the secret) 
+7. Notes - This is the Description field in the Passphrase entry.
+8. Creditial-type - This is the Passphrase entry's `CredentialType`, I only included the 5 types I found from my `mysqldump`. They are:
+    1. note
+    2. ssh-generated-key
+    3. ssh-key-text
+    4. token
+    5. password
+
+If the Credential-type is not `password`, then the imported 1Password entry's `password` field will be blank. Instead, an original field with the same name as the Credential-type will contain the secret. 
+
+## Handling escape backslashes, double and single quotes
+
+I have tried my best to correctly handle the escape backslashes, double and single quotes. I think I got it right, but if your import turns out looking wrong, let me know. 
--- passphrase_to_1password.py
+++ passphrase_to_1password.py
@@ -5,12 +5,15 @@ from subprocess import call
 SINGLE_QUOTE_PLACEHOLDER = "@SINGLE_placeholder"
 DOUBLE_QUOTE_PLACEHOLDER = "@DOUBLE_placeholder"
 
-# list of fields in the CSV output file, we use it to validate the added field names in the login class
-csv_fields = ["Title","Login URL","Login Username","Login Password","Created","Modified",
-              "Author","Notes","credential-type","note","ssh-generated-key","ssh-key-text","token"]
+# list of fields in the CSV output file, we use it to validate the added field
+# names in the login class
+csv_fields = ["Title","Login URL","Login Username",
+              "Login Password","Created","Modified",
+              "Author","Notes","credential-type","note",
+              "ssh-generated-key","ssh-key-text","token"]
 
 def parseOptions():
-    parser = argparse.ArgumentParser(description="Imports sql dump from Phabricator to extract passphrase for importing into 1Password")
+    parser = argparse.ArgumentParser(description="Imports mysqldump from Phabricator to extract passphrase for importing into 1Password")
 
     parser.add_argument("-p", "--passphrase_sql_file", 
         help="The passphrase SQL file",
@@ -36,7 +39,18 @@ def parseOptions():
 
     return args
 
+# load_table looks for an SQL table in the SQL file and loads the data into a
+# list of dictionaries (one dictionary per line) if the index_column is not
+# given. If the index_column is given, then it will return a dictionary of
+# dictionaries with the corresponding index_column value as the key to each
+# line.
 def load_table(sql_filename, table_name, index_column=None):
+    STATE_LOOK_FOR_TABLE = 0
+    STATE_LOOK_FOR_FIELDS = 1
+    STATE_LOOK_FOR_DATA = 2
+    STATE_COMPLETE = 3
+    state = STATE_LOOK_FOR_TABLE
+
     if index_column:
         #if we specify an index column, we want a dictionary
         data = {}
@@ -45,50 +59,64 @@ def load_table(sql_filename, table_name, index_column=None):
         data = []
     try:
         with open(sql_filename, "r") as f:
-            tableFound = False
+
             table_fields = []
-            table_fields_end = False
+
+            # loop through the SQL file with a little state machine that looks
+            # for the create table statement, then detects every field in the
+            # table, and finally, it looks for the insert statement for the
+            # table and grabs all the data into dictionary for each line of 
+            # data.  
             for line in f:
-                if not tableFound:
+                # first thing is try to find the table
+                if state == STATE_LOOK_FOR_TABLE:
                     matchObj = re.match('CREATE TABLE `('+table_name+')`',line)
                     if matchObj:
-                        tableFound = True
+                        # we have found the table we were looking for
+                        state += 1
                         logging.info("Table found: %s"%matchObj.group(1))
-                else:
-                    if not table_fields_end:
-                        matchObj = re.match('\A +`(\w+)`', line)
-                        if matchObj:
-                            table_fields.append(matchObj.group(1))
-                            logging.debug("Field found: %s"%matchObj.group())
-                        else:
-                            matchObj = re.match('\A\)',line)
-                            if matchObj:
-                                table_fields_end = True
+                elif state == STATE_LOOK_FOR_FIELDS:
+                    # after find the table, we need to check if we have all the
+                    # fields in the table recorded
+                    matchObj = re.match(r'\A +`(\w+)`', line)
+                    if matchObj:
+                        # found a table field, append it to the
+                        # table_fields list
+                        table_fields.append(matchObj.group(1))
+                        logging.debug("Field found: %s"%matchObj.group())
                     else:
-                        matchObj = re.match('INSERT INTO `'+table_name+'` VALUES (.+)', line)
+                        # check if we found the end of the fields in the
+                        # CREATE TABLE statement
+                        matchObj = re.match(r'\A\)',line)
                         if matchObj:
-                            table_values = matchObj.group(1).lstrip('(').rstrip(';)').replace('),(','\n').replace("	","    ")
-                            # replace /' with SINGLE_QUOTE_PLACEHOLDER but not //'
-                            table_values = re.sub(r"([^\\])(\\')",r"\1"+SINGLE_QUOTE_PLACEHOLDER,table_values)
-                            # replace /" with DOUBLE_QUOTE_PLACEHOLDERe but not //"
-                            table_values = re.sub(r'([^\\])(\\")',r"\1"+DOUBLE_QUOTE_PLACEHOLDER,table_values)
-                            value_reader = csv.reader(table_values.splitlines(), quotechar="'", quoting=csv.QUOTE_MINIMAL)
-                            for row in value_reader:
-                                count = 0
-                                new_dict = {}
-                                for column in row:
-                                    try:
-                                        #new_dict[table_fields[count]] = column.replace("@han_single_quote","'").replace("@han_double_quote","\"")
-                                        new_dict[table_fields[count]] = column
-                                    except IndexError as e:
-                                        logging.error(row)
-                                        raise
-                                    count+=1
-                                if index_column:
-                                    data[new_dict[index_column]] = copy.deepcopy(new_dict)
-                                else:
-                                    data.append(copy.deepcopy(new_dict))
-                            break                     
+                            # yes, we found the end of the fields, so move to
+                            # next state
+                            state += 1
+                elif state == STATE_LOOK_FOR_DATA:
+                    matchObj = re.match(r'INSERT INTO `'+table_name+'` VALUES (.+)', line)
+                    if matchObj:
+                        # we found the INSERT INTO statement for this table, 
+                        # strip off first and last brackets and the semicolon
+                        # Also, find any tab characters and just replace them 
+                        # with four spaces. We split each item into its own
+                        # line, so that we can use csv.DictReader to consume
+                        # the whole thing for us.
+                        table_values = matchObj.group(1).lstrip('(').rstrip(';)').replace('),(','\n').replace("	","    ")
+                        # replace /' with SINGLE_QUOTE_PLACEHOLDER but not //'
+                        table_values = re.sub(r"([^\\])(\\')",r"\1"+SINGLE_QUOTE_PLACEHOLDER,table_values)
+                        # replace /" with DOUBLE_QUOTE_PLACEHOLDERe but not //"
+                        table_values = re.sub(r'([^\\])(\\")',r"\1"+DOUBLE_QUOTE_PLACEHOLDER,table_values)
+
+                        reader = csv.DictReader(table_values.splitlines(), fieldnames=table_fields, quotechar="'", quoting=csv.QUOTE_MINIMAL)
+                        for row in reader:
+                            if index_column:
+                                data[row[index_column]] = copy.deepcopy(row)
+                            else:
+                                data.append(copy.deepcopy(row))
+    
+                        # we have reached the end of the data import for this table
+                        state += 1
+                        break
             f.close()
     except IOError:
         logging.error("Failed to read file %s" % sql_filename)
@@ -98,13 +126,21 @@ def load_table(sql_filename, table_name, index_column=None):
         logging.error(traceback.format_exc())
         raise
 
+    if state < STATE_COMPLETE:
+        if state == STATE_LOOK_FOR_TABLE:
+            sys.exit("Table '%s' not found in %s."%(table_name,sql_filename))
+        elif state ==  STATE_LOOK_FOR_FIELDS:
+            sys.exit("Something went wrong, I couldn't find the end of the fields for table '%s' in %s."%(table_name, sql_filename))
+        elif state == STATE_LOOK_FOR_DATA:
+            sys.exit("We couldn't load the data for table '%s' in %s."%(table_name, sql_filename))
+
     return data
 
 class login():
     # mainly, this class was created so we can cross-check that any field names
     # added to the entry are in the required_cvs_fields list. It is to prevent
-    # me adding a new field name, but forget to update the cvs_fields list that
-    # is passed to csv.dictWriter
+    # me adding a new field name, but forgetting to update the cvs_fields list
+    # that is passed to csv.DictWriter
     def __init__(self, required_cvs_fields=[]):
         self.required_cvs_fields = required_cvs_fields
 
